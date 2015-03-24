aa2a45af84ec87c1be65b9667addc796f12d16fd
Chris Weyl <cweyl@alumni.drew.edu>
2015-03-23 18:07:51 -0700

    debian/: nuke unused example files generated by dh_make

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ndn-perl-modules):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ndn-perl-modules" command="/usr/bin/ndn-perl-modules"
--- debian/ndn-perl-modules.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ndn-perl-modules package
-#
-0 4	* * *	root	[ -x /usr/bin/ndn-perl-modules_maintenance ] && /usr/bin/ndn-perl-modules_maintenance
